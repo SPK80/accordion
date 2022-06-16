7d85977023df1cbdf80efd8968303e85e0245f6b
SPK80 <psukonkin@gmail.com>
2022-06-16 12:35:54 +0300

    feat: implement Accordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from "./components/Accordion";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Accordion title={"title"}>
+                <Accordion title={"1"}>
+                    <div>
+                        1
+                    </div>
+                </Accordion>
+                <Accordion title={2}>
+                    <Accordion title={"2.1"}>
+                        2.1
+                    </Accordion>
+                </Accordion>
+            </Accordion>
+        </div>
+    );
 }
 
 export default App;
